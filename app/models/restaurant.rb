97540d3487f17c8fb38dc1c666517d4365fb1cc3
kauana tombolato <kauanatomb@gmail.com>
2023-05-24 15:32:05 +0200

    add restaurants and reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+    has_many(:reviews, dependent: :destroy)
+    validates :name, :address, :category, presence: true
+    validates :phone_number, presence: true
+    validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
 end
